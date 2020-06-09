2efeef7be188756fdf1b20946d41abe9aea2f27b
quynh cao <quynhcao@quynhs-MacBook-Pro.local>
2020-06-09 12:44:40 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Button, Form, FormGroup, Label, Input}
+from 'reactstrap';
+
+
+class App extends Component{
+  render() {
+    return(
+        <Form className="login-form">
+            <h1>
+                <span className="font-weight-bold">Login</span>
+            </h1>
+           <FormGroup>
+               <Label>Username</Label>
+               <Input type="username" placeholder="Username"/>
+           </FormGroup>
+            <FormGroup>
+                <Label>Password</Label>
+                <Input type="password" placeholder="Password"/>
+            </FormGroup>
+
+            <Label> Remember me? </Label>
+
+            <Button className="btn-lg btn-dark btn-block">
+                    Sign in
+                </Button>
+           <div className="text-center pt-3">New around here? Sign up</div>
+            <div className="text-center pt-3">Forgot password?</div>
+
+
+        </Form>
+    );
+  }
 }
 
 export default App;
